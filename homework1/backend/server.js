59688ba6f056b7c441ace3410ea385c204a6109c
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-30 21:45:51 +0700

    save

--- homework1/backend/server.js
+++ homework1/backend/server.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.listen(3000 , ()=>{
+    console.log("SERVER START PORT ===> 3000");
+})
